860f187b31396099925510a83269106fe2d036d1
yanshg <yanshg@yahoo.com>
2020-08-30 11:14:32 -0700

    Add hashmap_chaining.py

---
+++ Hashmap/hashmap_chaining.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python
+
+# Seperate chaining with linked list
+#
+# 1. Require only basic data structures with simple algorithms,
+# 2. Can use simple hash functions that are unsuitable for other methods
+# 3. Remain effective even when the number of table entries n is much higher than the number of buckets.
+
+# For separate-chaining, the worst-case scenario is when all entries are inserted into the same bucket,
+#
+# The bucket chains are often searched sequentially using the order the entries were added to the bucket.
+# so usually when the entries exceed 8, then use balanced binary search tree, like red-black tree to
+#
+# Disadvantages:
+#
+# 1. When storing small keys and values, the space overhead of the next pointer in each entry record can be significant.
+# 2. Traversing a linked list has poor cache performance, making the processor cache ineffective.
+
+CAPACITY = 512
+
+import hashlib
+
+class Node:
+    def __init__(self,key,value,prev=None,next=None):
+        self.key = key
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __repr__(self):
+        node = self.head
+        string = '#'
+        while node:
+            #string += '[{}, {}] '.format(node.key, node.value)
+            string += '{},'.format(node.key)
+            node = node.next
+        string += '#'
+
+        return string
+
+    def append(self, node):
+        if self.tail:
+            node.prev = self.tail
+            node.next = None
+            self.tail.next = node
+
+        self.tail = node
+        if not self.head:
+            self.head = node
+
+    def delete(self, node):
+        if not node:
+            return
+
+        prev = node.prev
+        next = node.next
+        if prev:
+            prev.next = next
+        if next:
+            next.prev = prev
+
+        if self.head == node:
+            self.head = next
+        if self.tail == node:
+            self.tail = prev
+
+    def get_node(self, key):
+        node = self.head
+        while node and node.key != key:
+            node = node.next
+        return node
+
+class HashMap:
+    def __init__(self,capacity=CAPACITY):
+        self.capacity = capacity
+        self.array = [ 0 for i in range(capacity) ]
+        self.count = 0
+
+    def _hash(self, key):
+        hash = int(hashlib.sha1(str(key).encode('utf-8')).hexdigest(), 16)
+        return hash % len(self.array)
+
+    # return (index for the exist key, index which available for the key, Node pointer)
+    def _get_index_node(self, key):
+        index = self._hash(key)
+        linkedlist = self.array[index]
+        node = linkedlist.get_node(key) if linkedlist else None
+        return (index, node)
+
+    def exists(self, key):
+        index,node = self._get_index_node(key)
+        return bool(node)
+
+    def get(self, key):
+        index,node = self._get_index_node(key)
+        return node.value if node else None
+
+    def put(self, key, value):
+        index,node = self._get_index_node(key)
+        if node:
+            node.value = value
+            return
+
+        if not self.array[index]:
+            self.array[index] = DoubleLinkedList()
+
+        self.array[index].append(Node(key, value))
+        self.count += 1
+
+    def delete(self, key):
+        index,node = self._get_index_node(key)
+        if node:
+            self.array[index].delete(node)
+            self.count -= 1
+
+    def load_factor(self):
+        return float(self.count)/self.capacity
+
+if __name__ == '__main__':
+    h = HashMap()
+    h.put(123, 4)
+    h.put('k1', 'v1')
+    h.put('k1', 'v1_new')
+    h.put('k2', 'v2')
+    assert h.get('k1') == 'v1_new'
+    assert h.get(123) == 4
+
+    h.put('k3', 'v3')
+    assert h.get('k3') == 'v3'
+
+    h.delete('k4')
+
+    h.delete('k3')
+    #assert not h.get('k3')
+
+    for i in range(1512):
+        h.put(i, i)
+
+    print(h.array)
+
+    print("load factor: ", h.load_factor())
--- Hashmap/hashmap_openaddressing.py
+++ Hashmap/hashmap_openaddressing.py
@@ -7,27 +7,64 @@
 #
 # Article: https://en.wikipedia.org/wiki/Linear_probing
 #
+
+# Open addressing: all entry records are stored in the bucket itself.
+# but the location ("address") of the item is not determined by its hash value.
+#
+# Collision solutions: use probe sequences like linear probing, quadratic probing, double hashing
+#
+# For Linear probling: https://en.wikipedia.org/wiki/Linear_probing
+#
 # 1. Each cell of a hash table stores a single key-value pair.
 # 2. When the hash function causes a collision by mapping a new key to a cell of the hash table that is already occupied by another key, linear probing searches the table for the closest following free location and inserts the new key there.
 # 3. Lookups are performed in the same way, by searching the table sequentially starting at the position given by the hash function, until finding a cell with a matching key or an empty cell.
 
+# Hash function selections:
+#
+# 1. Provide a uniform distribution of hash values. difficult to ensure by design, but may be evaluated by statistical tests.
+# 2. For open addressing, need avoid clustering.
+# 3. Cryptograghic hash function provide good hash functions for any table size by modulo reduction or by bit masking. but offen slower to compute. a non-cryptographic hash function might be preferable, like DJB2, FNV1a, etc.
+
+# Key statistics:
+#
+# Load factor = n/k
+#
+# n: the number of occupied entries in the hash table
+# n: the number of all buckets
+#
+# As the load factor grows larger, the hash table becomes slower, and it may even fail to work.
+
 CAPACITY = 256
 
 LINEAR_PROBING=0
+QUADRATIC_PROBING=1
 DOUBLE_HASH=2
+
+import sys,hashlib
+
+if sys.version_info[0] == 3:
+    _get_byte = lambda c:c
+else:
+    _get_byte = ord
 
 class HashMap:
     def __init__(self,capacity=CAPACITY,probe_mode=LINEAR_PROBING):
         self.capacity = capacity
         self.array = [ [] for i in range(capacity) ]
         self.probe_mode = probe_mode
+        self.count = 0
 
     # DJB2 Hash algorithm
     def _hash(self, key):
         bs = str(key).encode('utf-8')
-        hash =  5381
-        for b in bs:
-            hash = (hash * 33) + b
+
+        use_simple_hash_function = False
+        if use_simple_hash_function:
+            hash =  5381
+            for b in bs:
+                hash = (hash * 33) + _get_byte(b)
+        else:
+            hash = int(hashlib.md5(bs).hexdigest(), 16)
         return hash % len(self.array)
 
     # return (index for the exist key, index which available for the key)
@@ -41,9 +78,10 @@ class HashMap:
             if self.array[index][0] == key:
                 return (index, index)
 
+            # Linear probing
             index = (index + 1) % len(self.array)
             if index == orig_index:
-                return (-1, -1)
+                break
 
         return (-1, -1)
 
@@ -64,11 +102,16 @@ class HashMap:
             self.array[exist_index][1] = value
         elif avail_index != -1:
             self.array[avail_index] = [key, value]
+            self.count += 1
 
     def delete(self, key):
         exist_index,_  = self._get_index(key)
         if exist_index != -1:
             self.array[exist_index] = []
+            self.count -= 1
+
+    def load_factor(self):
+        return float(self.count)/self.capacity
 
 if __name__ == '__main__':
     h = HashMap()
@@ -87,7 +130,10 @@ if __name__ == '__main__':
     h.delete('k3')
     assert not h.get('k3')
 
+    print("load factor: ", h.load_factor())
+
     for i in range(512):
         h.put(i, i)
 
+    print("load factor: ", h.load_factor())
     print(h.array)
