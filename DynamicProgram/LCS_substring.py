adfd48416167adab45fd50c74fdc62e422c07c94
yanshg <yanshg@yahoo.com>
2020-09-16 10:08:50 -0700

    update LCS_substing.py

--- DynamicProgram/LCS_substring.py
+++ DynamicProgram/LCS_substring.py
@@ -19,18 +19,34 @@
 def get_lcs_substring(X, Y):
     m,n = len(X),len(Y)
     max_len = 0
-    substr = ''
-    DP = [ [''] * (n+1) for i in range(m+1) ]
+    ending_index = 0
+    DP = [ [0] * (n+1) for i in range(m+1) ]
     for i in range(1,m+1):
         for j in range(1,n+1):
-            DP[i][j] = ''
             if X[i-1] == Y[j-1]:
-                DP[i][j] = DP[i-1][j-1] + X[i-1]
-                l = len(DP[i][j])
-                if l > max_len:
-                    max_len = l
-                    substr = DP[i][j]
-    return substr
+                DP[i][j] = DP[i-1][j-1] + 1
+                if DP[i][j] > max_len:
+                    max_len = DP[i][j]
+                    ending_index = j
+
+    return Y[ending_index-max_len:ending_index]
+
+# Simplified to one dimension array
+def get_lcs_substring2(X, Y):
+    m,n = len(X),len(Y)
+    max_len = 0
+    ending_index = 0
+    DP = [0] * (n+1)
+    for i in range(1,m+1):
+        for j in range(n,0,-1):
+            if X[i-1] == Y[j-1]:
+                DP[j] = DP[j-1] + 1
+                if DP[j] > max_len:
+                    max_len = DP[j]
+                    ending_index = j
+
+    return Y[ending_index-max_len:ending_index]
 
 assert get_lcs_substring('ABABC','BABCA') == 'BABC'
+assert get_lcs_substring2('ABABC','BABCA') == 'BABC'
 
