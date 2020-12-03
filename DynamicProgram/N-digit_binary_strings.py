d4a31e789fca6dcff59338ca3acd71debaae3107
yanshg <yanshg@yahoo.com>
2020-12-02 23:17:39 -0800

    Add N-digit_binary_strings.py

---
+++ DynamicProgram/N-digit_binary_strings.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+'''
+
+Find all N-digit binary strings without any consecutive 1's
+
+For example, for N = 5 the binary strings satisfies the given constraints are
+
+00000
+00001
+00010
+00100
+00101
+01000
+01001
+01010
+10000
+10001
+10010
+10100
+10101
+
+total 13 strings.
+
+'''
+
+# use DP table
+#
+# DP[i][0]: means counts of N-binary string which i-th digit is 0
+# DP[i][0]: means counts of N-binary string which i-th digit is 1
+#
+# Base cases:
+#       DP[n-1][0] = 1
+#       DP[n-1][1] = 1
+#
+# Result:   DP[0][0] + DP[0][1]
+
+def countStrings(n):
+
+    DP = [ [1,1] for i in range(n) ]
+    for i in range(n-2,-1,-1):
+        DP[i][0] = DP[i+1][0] + DP[i+1][1]
+        DP[i][1] = DP[i+1][0]
+
+    return DP[0][0] + DP[0][1]
+
+assert countStrings(5) == 13
