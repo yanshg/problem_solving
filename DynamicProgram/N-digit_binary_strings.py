a5b80e13e2af9ff3f8dff0e167f6af96044fca08
yanshg <yanshg@yahoo.com>
2020-12-05 18:31:13 -0800

    update N-digit_binary_strings.py

--- DynamicProgram/N-digit_binary_strings.py
+++ DynamicProgram/N-digit_binary_strings.py
@@ -36,7 +36,6 @@ total 13 strings.
 # Result:   DP[0][0] + DP[0][1]
 
 def countStrings(n):
-
     DP = [ [1,1] for i in range(n) ]
     for i in range(n-2,-1,-1):
         DP[i][0] = DP[i+1][0] + DP[i+1][1]
@@ -44,4 +43,17 @@ def countStrings(n):
 
     return DP[0][0] + DP[0][1]
 
+def get_all_strings(n):
+    DP = [ [[],[]] for i in range(n) ]
+    DP[-1]=[['0'],['1']]
+
+    for i in range(n-2,-1,-1):
+        DP[i][0] = [ '0'+item for item in DP[i+1][0] + DP[i+1][1] ]
+        DP[i][1] = [ '1'+item for item in DP[i+1][0] ]
+
+    result = DP[0][0] + DP[0][1]
+    #print(result)
+    return result
+
 assert countStrings(5) == 13
+assert get_all_strings(5) = ['00000', '00001', '00010', '00100', '00101', '01000', '01001', '01010', '10000', '10001', '10010', '10100', '10101']
